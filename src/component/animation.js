66ac4a42d2bcc32a2ac45ea738b3855897059c65
harsh08881 <silentkiller9369@gmail.com>
2025-01-28 12:28:23 +0530

    add

--- src/component/animation.js
+++ src/component/animation.js
@@ -3,46 +3,14 @@ import "./animation.css";
 
 const TransitionShowcase = () => {
   return (
-    <div className="main">
-      <div className="container">
-        <div className="bus-details">
-          <div className="flex">
-            <div className="sa">
-            <img
-              src={require('../assets/air-conditioner-icon.png')}
-              alt="Example placeholder"
-            />
-            <span>AC Bus</span>
-            </div>
-          </div>
-          <div className="flex">
-            <img
-              src={require("../assets/travel_agency.png")}
-              alt="Example placeholder"
-            />
-            <span>Yuvraj Bus Travels
-            </span>
-          </div>
-          <div className="flex">
-            <img
-              src={require("../assets/list_bus.png")}
-              alt="Example placeholder"
+   <>
+    <div class="child">
+    <div class="child1">Div 2</div>
+    <div class="child1">Div 3</div>
 
-            />
-            <span>Non Sleeper AC</span>
-          </div>
-          <div className="flex">
-            <img
-              src={require("../assets/timing.png")}
-              alt="Example placeholder"
-            />
-            <span> 6H 36M</span>
-          </div>
-        </div>
-        <div className="bus-time"></div>
-        <div className="bus-price"></div>
-      </div>
     </div>
+    
+    </> 
   );
 };
 
