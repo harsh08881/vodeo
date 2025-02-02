2e0d0ac0694e705dbf8b2f590f6ca5fa8c6d7042
harsh08881 <silentkiller9369@gmail.com>
2025-02-03 00:23:32 +0530

    add navbar

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import './Header.css';
-import logo from '../../assets/logo.png'
+import React , { useState }from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import "./Header.css";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
     <header className="header">
       <div className="header-container">
@@ -11,15 +17,25 @@ const Header = () => {
           <img src={logo} alt="App Logo" className="header-logo-img" />
           <span>Vodeo</span>
         </div>
-        <nav className="header-nav">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/about" className="nav-link">About</Link>
-          <Link to="/vid" className="nav-link">Start a Call</Link> {/* Link to /vid */}
-          <Link to="/profile" className="nav-link">Profile</Link>
+        <nav className={`header-nav ${isOpen ? "show" : ""}`}>  
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/vid" className="nav-link">
+            Start a Call
+          </Link>{" "}
+          {/* Link to /vid */}
+          <Link to="/profile" className="nav-link">
+            Profile
+          </Link>
         </nav>
         <div className="header-actions">
           <button className="login-btn">Login</button>
           <button className="signup-btn">Sign Up</button>
+          <span class="material-symbols-outlined"  onClick={toggleNavbar}  style={{ cursor: "pointer", color: "#3f4a55", fontWeight: "bold" }}>menu</span>
         </div>
       </div>
     </header>
